7483bcc8728881f2930a1c0d46db18699804d6d2
GDKID <bawbeefarted@gmail.com>
2023-01-23 18:08:21 -0500

    Add shutdown hook

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,24 @@
 import com.github.kwhat.jnativehook.GlobalScreen;
 
 public class Main {
+    static void onShutDown() {
+        try { GlobalScreen.unregisterNativeHook(); }
+        catch (Exception e) { e.printStackTrace(); }
+    }
+
     public static void main(String[] args) {
         try {
             GlobalScreen.registerNativeHook();
         }
-        catch (Exception ex) {
+        catch (Exception e) {
             System.err.println("There was a problem registering the native hook.");
-            System.err.println(ex.getMessage());
+            System.err.println(e.getMessage());
 
             System.exit(1);
         }
 
         // start main menu
         MCBs.openHome();
+        Runtime.getRuntime().addShutdownHook(new Thread(Main::onShutDown));
     }
 }
